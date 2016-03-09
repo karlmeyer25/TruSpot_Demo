82f38a40133fc79b76fb6dfa5a5b19016c4e6329
Stefan Tsekov <stefan.tsekov@dision.co>
2016-03-09 12:52:59 +0200

    #16 Show map on full screen with animation, support both portrait and landscape modes

--- app/src/main/java/com/truspot/android/activities/VenueActivity.java
+++ app/src/main/java/com/truspot/android/activities/VenueActivity.java
@@ -1,19 +1,18 @@
 package com.truspot.android.activities;
 
-import android.animation.Animator;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -25,9 +24,10 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.rey.material.widget.Button;
 import com.truspot.android.R;
-import com.truspot.android.constants.Constants;
+import com.truspot.android.animations.ViewMover;
 import com.truspot.android.utils.GoogleUtil;
 import com.truspot.android.utils.LocationUtil;
+import com.truspot.android.utils.LogUtil;
 import com.truspot.android.utils.Util;
 import com.truspot.backend.api.model.VenueFull;
 
@@ -50,23 +50,32 @@ public class VenueActivity
     private static final String BUNDLE_VF = "vf";
     private final static String BUNDLE_KEY_MAP_STATE = "venue_map_data";
 
+    private static final int MODE_NORMAL = 1;
+    private static final int MODE_EXTENDED_MAP = 2;
+
+    private static final int ANIM_DURATION = 400;
+
     // variables
     private VenueFull mVf;
     private GoogleMap mGoogleMap;
-    private boolean mIsMapOnFullScreen;
     private Resources mRes;
     private int mScreenWidth;
     private int mScreenHeight;
+    private boolean mContentLayoutKnown;
+    private boolean mMapInitialStateReady;
+    private int mContentWidth;
+    private int mContentHeight;
+    private int mToolbarHeight;
+    private int mCurrMode;
+    private ViewMover mViewMover;
 
     // UI
     @Bind(R.id.toolbar_activity_venue)
     Toolbar toolbar;
     @Bind(R.id.mv_activity_venue)
     MapView mv;
-    @Bind(R.id.nsv_activity_venue)
-    NestedScrollView nsv;
-    @Bind(R.id.abl_activity_venue)
-    AppBarLayout abl;
+    @Bind(R.id.rl_activity_venue_container)
+    RelativeLayout rlContainer;
     @Bind(R.id.btn_activity_venue_show_social_media)
     Button btnShowSocialMedia;
     @Bind(R.id.tv_activity_venue_name)
@@ -150,15 +159,6 @@ public class VenueActivity
     }
 
     @Override
-    public void onBackPressed() {
-        if (mIsMapOnFullScreen) {
-            animateMap(false);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home: {
@@ -169,6 +169,15 @@ public class VenueActivity
             default: {
                 return super.onOptionsItemSelected(item);
             }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mCurrMode != MODE_NORMAL) {
+            showMode(MODE_NORMAL, true);
+        } else {
+            super.onBackPressed();
         }
     }
 
@@ -189,9 +198,13 @@ public class VenueActivity
 
         mScreenWidth = Util.getScreenDimensions(this).x;
         mScreenHeight = Util.getScreenDimensions(this).y;
+
+        mCurrMode = MODE_NORMAL;
     }
 
     private void initListeners() {
+        final String TAG = Util.stringsToPath(BASIC_TAG, "initListeners");
+
         btnShowSocialMedia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -201,6 +214,41 @@ public class VenueActivity
                     e.printStackTrace();
                 }
             }
+        });
+
+        rlContainer.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @Override
+            public void onGlobalLayout() {
+                LogUtil.log(TAG, "is called");
+
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    rlContainer.getViewTreeObserver()
+                            .removeGlobalOnLayoutListener(this);
+                } else {
+                    rlContainer.getViewTreeObserver()
+                            .removeOnGlobalLayoutListener(this);
+                }
+
+                mContentLayoutKnown = true;
+                mContentWidth = rlContainer.getMeasuredWidth();
+                mContentHeight = rlContainer.getMeasuredHeight();
+                mToolbarHeight = toolbar.getMeasuredHeight();
+
+                if (!mMapInitialStateReady) {
+                    checkMapReadyForInit();
+                }
+            }
+        });
+
+        rlContainer.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // consume all touch events, otherwise it will move the map
+                return true;
+            }
+
         });
     }
 
@@ -227,8 +275,66 @@ public class VenueActivity
         mv.getMapAsync(this);
     }
 
-    private void loadVenueOnMap() {
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
+        checkMapReadyForInit();
+    }
+
+    private void checkMapReadyForInit() {
+        if (mGoogleMap != null &&
+                mContentLayoutKnown) {
+            mMapInitialStateReady = true;
+
+            // init map settings
+            mGoogleMap.setMyLocationEnabled(true);
+
+            mGoogleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+                @Override
+                public void onMapClick(LatLng latLng) {
+                    if (mCurrMode == MODE_NORMAL) {
+                        showMode(MODE_EXTENDED_MAP, true);
+                    }
+                }
+
+            });
+
+            boolean isPortrait = Util.isPortraitOrientation(mRes);
+
+            mViewMover = new ViewMover(ANIM_DURATION);
+            mViewMover.addObjectToMove(
+                    new ViewMover.MoverObject(
+                            rlContainer,
+                            0, 0,
+                            isPortrait ? mContentHeight : 0,
+                            isPortrait ? 0 : -mContentWidth)
+            ).addObjectToMove(
+                    new ViewMover.MoverObject(toolbar, 0, 0, -mToolbarHeight, 0));
+
+            showMode(mCurrMode, false);
+        }
+    }
+
+    private void showMode(int mode, boolean withAnimation) {
         mGoogleMap.clear();
+        mCurrMode = mode;
+
+        UiSettings uiSettings = mGoogleMap.getUiSettings();
+
+        uiSettings.setCompassEnabled(false);
+        uiSettings.setRotateGesturesEnabled(false);
+        uiSettings.setMyLocationButtonEnabled(mCurrMode == MODE_EXTENDED_MAP);
+        uiSettings.setZoomControlsEnabled(mCurrMode == MODE_EXTENDED_MAP);
+
+        mViewMover.move(mCurrMode == MODE_NORMAL, withAnimation);
+
+        boolean isPortrait = Util.isPortraitOrientation(mRes);
+        int area = isPortrait ? mScreenHeight : mScreenWidth;
+        int notVisibleArea = mCurrMode == MODE_NORMAL ? (isPortrait ? mContentHeight : mContentWidth) : 0;
+
+        mGoogleMap.setPadding(isPortrait ? 0 : notVisibleArea, 0, 0, isPortrait ? notVisibleArea : 0);
 
         LocationUtil.addVenueMarker(this, mGoogleMap, mVf.getVenue(), mVf.getVenue().getCapacity());
 
@@ -236,68 +342,24 @@ public class VenueActivity
 
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(
                 venueLatLng,
-                Constants.DEFAULT_CAMERA_ZOOM);
+                calculateZoom(mGoogleMap, area, notVisibleArea));
 
-        mGoogleMap.moveCamera(cameraUpdate);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mGoogleMap = googleMap;
-
-        // map settings
-        UiSettings settings = mGoogleMap.getUiSettings();
-
-        settings.setMyLocationButtonEnabled(false);
-        settings.setZoomControlsEnabled(false);
-
-        mGoogleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                if (!mIsMapOnFullScreen) {
-                    animateMap(true);
-                }
-            }
-        });
-
-        loadVenueOnMap();
-    }
-
-    private void animateMap(final boolean showOnFullScreen) {
-        float translationY;
-        boolean isPortrait = Util.isPortraitOrientation(mRes);
-
-        if (isPortrait) {
-            translationY = mScreenHeight - Util.convertDpiToPixels(this, 300);
+        if (withAnimation) {
+            mGoogleMap.animateCamera(cameraUpdate, ANIM_DURATION, null);
         } else {
-            translationY = mScreenWidth - Util.convertDpiToPixels(this, 300);
+            mGoogleMap.moveCamera(cameraUpdate);
         }
-
-        nsv.animate().translationYBy(showOnFullScreen ? translationY : -translationY).setListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-                abl.setLayoutParams(new CoordinatorLayout.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        showOnFullScreen ?
-                                ViewGroup.LayoutParams.MATCH_PARENT :
-                                Util.convertDpiToPixels(VenueActivity.this, 300)));
-                abl.setExpanded(true, true);
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                mIsMapOnFullScreen = showOnFullScreen;
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
     }
+
+    private float calculateZoom(GoogleMap map, int area, int notVisibleArea) {
+        final String TAG = Util.stringsToPath(BASIC_TAG, "calculateZoom");
+        float minZoomLevel = Math.max(map.getMinZoomLevel(), 9);
+        float maxZoomLevel = Math.min(map.getMaxZoomLevel(), 15);
+        float calculated = (((float) (area - notVisibleArea) / (float) area) * (maxZoomLevel - minZoomLevel)) + minZoomLevel;
+
+        //LogUtil.log(TAG, String.format("maxZoomLevel: %s, calculated: %s", maxZoomLevel, calculated));
+
+        return calculated;
+    }
+
 }
